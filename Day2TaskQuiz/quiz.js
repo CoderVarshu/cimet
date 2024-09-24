50e13e1ac94bf5291b84d881e852439a359a1d51
varsha <bhardwajvarsha49@gmail.com>
2024-09-24 17:24:13 +0530

    Changes Added

--- Day2TaskQuiz/quiz.js
+++ Day2TaskQuiz/quiz.js
@@ -1,93 +1,62 @@
-const quizQuestions = [
-  {
-    question: "What is the capital of India",
-    answer: ["Jaipur", "Gujrat", "Delhi", "Rajsthan"],
-    correct: "Delhi",
-  },
-  {
-    question: "2 * 2 - 1",
-    answer: [1, 5, 3, 0],
-    correct: 3,
-  },
-  {
-    question: "1 + 4 * ? = 10",
-    answer: [1, 5, 2, 6],
-    correct: 2,
-  },
-  {
-    question: "What is the capital of MP ",
-    answer: ["Jaipur", "Bhopal", "Delhi", "Rajsthan"],
-    correct: "Bhopal",
-  },
-  {
-    question: "Who is Dropadi Murmu ",
-    answer: [
-      "CM of MP",
-      "President of india",
-      "Loksabha Speaker",
-      "none of these",
-    ],
-    correct: "President Of India",
-  },
-];
+import { quizQuestions } from './quizData.js'
+
 
 const timer = document.getElementById("timer");
 const question = document.querySelector(".QuestionContainer");
 const options = document.querySelector(".optionsConatiner");
 const button = document.querySelector("button");
+
+
 let score = 0;
 let count = 5;
 let questionCount = 1;
 const completedQuestionIndex = [];
 const shortedQuiz = quizQuestions.sort(() => 0.5 - Math.random())
-const totalQuestion = [...quizQuestions];
 
-let questionIndex = 1
-completedQuestionIndex.push(questionIndex)
+let questionIndex = 0
 
 function DisplayQuestion() {
   question.innerHtml = "";
 
-  question.innerHTML = `Question ${questionIndex + 1} : ${shortedQuiz[questionIndex].question}`;
+  question.innerHTML = `Question ${questionIndex + 1} : ${shortedQuiz[questionIndex]?.question}`;
 
   options.innerHTML = "";
-  
-  shortedQuiz[questionIndex].answer.forEach((option) => {
+
+  shortedQuiz[questionIndex]?.answer?.forEach((option) => {
 
     const button = document.createElement("button");
     button.textContent = option;
     button.setAttribute("class", "options");
     button.addEventListener("click", (e) => {
       if (e.target.innerText === quizQuestions[questionIndex].correct) score++;
-      questionCount++;
+       
     });
     options.appendChild(button);
   });
-  
-  let id = setInterval(() => {
 
-    console.log("Score", score, questionCount)
+  let id = setInterval(() => {
     if (count < 0) {
       clearInterval(id);
       count = 5;
       questionCount++;
       questionIndex++;
+      console.log("qcount",questionCount);
       DisplayQuestion();
-    } else if (questionCount > 5) {
-      displayScore();
-    }
+    } 
     timer.innerHTML = count--;
+    if (questionCount > 5) {
+      displayScore()}
+      
   }, 1000);
 
   function displayScore() {
+    clearInterval(id);
     question.innerHTML = "";
     timer.innerHTML = "";
     options.innerHTML = "";
     let h1 = document.createElement("h1");
     h1.textContent = `Your Score : ${score} / 5 `;
     options.append(h1);
-    console.log("SCORE", score, timer.innerHTML);
-    clearInterval(id);
   }
 
 }
--- Day2TaskQuiz/quizData.js
+++ Day2TaskQuiz/quizData.js
@@ -1,23 +1,32 @@
 export const quizQuestions = [
     {
-        question: "What is the capital of India",
-        answer : ['Jaipur', 'Gujrat', 'Delhi', 'Rajsthan' ],
+      question: "What is the capital of India",
+      answer: ["Jaipur", "Gujrat", "Delhi", "Rajsthan"],
+      correct: "Delhi",
     },
     {
-        question: 2 * 2 - 1,
-        answer : [ 1 , 5, 3, 0 ],
+      question: "2 * 2 - 1",
+      answer: [1, 5, 3, 0],
+      correct: 3,
     },
     {
-        question: 1 + 4 * '? = 10',
-        answer : [ 1, 5 , 2, 6],
-    },, 
-    {
-        question: "What is the capital of MP ",
-        answer : ['Jaipur', 'Bhopal', 'Delhi', 'Rajsthan' ],
+      question: "1 + 4 * ? = 10",
+      answer: [1, 5, 2, 6],
+      correct: 2,
     },
     {
-        question: "Who is Dropadi Murmu ",
-        answer : ['CM of MP', 'President of india', 'Loksabha Speaker', 'none of these' ],
+      question: "What is the capital of MP ",
+      answer: ["Jaipur", "Bhopal", "Delhi", "Rajsthan"],
+      correct: "Bhopal",
     },
-]
-
+    {
+      question: "Who is Dropadi Murmu ",
+      answer: [
+        "CM of MP",
+        "President of india",
+        "Loksabha Speaker",
+        "none of these",
+      ],
+      correct: "President Of India",
+    },
+  ];
